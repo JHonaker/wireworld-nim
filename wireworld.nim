37b4feb36255f4a8e2037a3fbb983459bea4b089
JHonaker <honaker215@gmail.com>
2017-05-21 00:10:39 -0400

    Changed World type from array[N, array[N, State]] to seq[State]

--- wireworld.nim
+++ wireworld.nim
@@ -2,31 +2,38 @@ type
   State* = enum
     ground, wire, head, tail
 
-  World*[N: static[int]] = array[N, array[N, State]]
+  World* = seq[State]
 
-proc `$`*[N](world: World[N]): string =
+const WorldSize = 75
+
+template get*(world: World, x, y: int): State =
+  world[x + WorldSize * y]
+
+proc newWorld*: World =
+  newSeq(result, WorldSize * WorldSize)
+  for idx in 0..< (WorldSize * WorldSize):
+      result[idx] = ground
+
+proc `$`*(world: World): string =
   result = ""
-  for row in world:
-    for col in row:
-      case col:
+  for y in 0..<WorldSize:
+    for x in 0..<WorldSize:
+      case world.get(x, y):
         of ground: result.add(" ")
         of wire: result.add("-")
         of head: result.add("+")
         of tail: result.add("=")
     result.add("\n")
 
-template get*[N](world: World[N], x, y: int): State =
-  world[y][x]
-
-proc neighbors[N](world: World[N], x, y: int): array[State, int] =
+proc neighbors(world: World, x, y: int): array[State, int] =
   # Returns a array with the count of each state in the neighboring cells
   result[world.get(x, y)] -= 1
   for i in [x - 1, x, x + 1]:
     for j in [y - 1, y, y + 1]:
-      if i >= 0 and i < N and j >= 0 and j < N:
+      if i >= 0 and i < WorldSize and j >= 0 and j < WorldSize:
         result[world.get(i, j)] += 1
 
-proc newState[N](world: World[N], x, y: int): State =
+proc newState(world: World, x, y: int): State =
   # Returns the new state for the cell after its update
   #
   # The update algorithm is:
@@ -50,22 +57,26 @@ proc newState[N](world: World[N], x, y: int): State =
       else:
         result = wire
 
-proc process*[N](world: var World[N]) =
+proc process*(world: var World) =
   # Updates the entire world according to the automata rules.
 
   # We must copy world first though, otherwise we'll get incorrect updating
   var newworld = world
-  for x in 0..<N:
-    for y in 0..<N:
+  for x in 0..<WorldSize:
+    for y in 0..<WorldSize:
       # Update the new world 
       newworld.get(x, y) = world.newState(x, y)
   # Update the original with the new states
   world = newworld
 
 if isMainModule:
-  var world: World[3] = [[wire, ground, head],
-                         [wire, ground, ground],
-                         [tail, head, head]]
+  var world = newWorld()
+  world.get(0, 0) = wire
+  world.get(2, 0) = head
+  world.get(0, 1) = wire
+  world.get(0, 2) = tail
+  world.get(1, 2) = head
+  world.get(2, 2) = head
 
   for _ in 1..10:
     echo world
